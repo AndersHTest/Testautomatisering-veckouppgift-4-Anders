32505a06a2d8e22b14bafd0a700fee2f1e964d15
AndersHTest <anders.henang@student.nbi-handelsakademin.se>
2026-09-08 20:43:21 +0200

    fixat linting.

--- src/insertion_sort/insertion_sort.py
+++ src/insertion_sort/insertion_sort.py
@@ -11,4 +11,3 @@ def insertion_sort(lst):
         if not inserted:
             result.append(item)
     return result
-
--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def sort_comparison_chart():
     size = ("2500", "5000", "7500", "10000", "12500")
     function = {
         'Insertion sort': (101.61, 413.50, 937.14, 1651.38, 2576.65),
-        'Merge sort': (4.44, 9.58, 14.86, 20.92, 26.45 ),
+        'Merge sort': (4.44, 9.58, 14.86, 20.92, 26.45),
 
     }
 
@@ -24,8 +24,9 @@ def sort_comparison_chart():
     ax.set_ylabel('Time (ms)')
     ax.set_xlabel('Storlek lista')
     ax.legend(loc='upper left', frameon=False, ncols=2)
-    ax.set_ylim(0,3000)
+    ax.set_ylim(0, 3000)
     plt.show()
+
 
 if __name__ == '__main__':
     sort_comparison_chart()
